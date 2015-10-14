06f19844ef569d2eb08845253fcdb5fcbb96bbeb
jenniferarce <arcej95@hotmail.com>
2015-10-14 01:54:26 -0300

    commit mapa

--- nexo.php
+++ nexo.php
@@ -46,6 +46,31 @@ switch ($queHago) {
 	case 'VerEnMapa':
 		include("partes/formMapa.php");
 	break;
+	case 'guardarMarcadores':
+        session_start();
+        if(isset($_POST["marcadores"]))
+        {
+            $filename = "ArchivosTxt/marcadores" . getdate()[0] . ".txt";
+
+            $_SESSION['file'] = $filename;
+            $puntos = $_POST["marcadores"];
+
+            $file = fopen($filename, "w");
+
+            foreach ($puntos as $valor)
+            {
+                $lat =  $valor["lat"];
+                $lng =  $valor["lng"];
+                $nombre =  $valor["nombre"];
+                fwrite($file, $lat.">".$lng.">".$nombre . PHP_EOL);
+            }
+        fclose($file);
+
+        echo "Marcadores guardados con exito";
+        }
+        else
+            echo "No ingreso marcador/es a guardar";
+        break;
 	/*case 'contarVotos':
 			$voto = new voto();
 			$voto->cont=$_POST['cont'];
